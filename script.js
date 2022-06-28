a78915d54f095c47355811c5045568b5116eb93a
NicFie <nicola.field2@gmail.com>
2022-06-28 16:13:59 +0100

    Have a working script with a remove button, and toggle slider for if book is read or not.

--- script.js
+++ script.js
@@ -13,37 +13,71 @@ function addBookToLibrary() {
     myLibrary.push(userInput);
 }
 
-
-
-
 function displayBooks() {
     for (let i = 0; i < myLibrary.length; i++) {
-        document.getElementById('title-input').innerHTML = myLibrary[i].title;
-        document.getElementById('author-input').innerHTML = myLibrary[i].author;
-        document.getElementById('pages-input').innerHTML = myLibrary[i].pages;
-        document.getElementById('read-input').innerHTML = myLibrary[i].read;
-        const card = document.getElementById('card');
-        const cardClone = card.cloneNode(true);
-        cardClone.classList.remove('template');
-        cardClone.classList.add('displayed-card');
-        const removeBtn = document.createElement('button');
-        removeBtn.textContent = 'Remove';
-        removeBtn.setAttribute('id', `${i}`);
-        removeBtn.addEventListener('click', () => {
-            myLibrary.splice(myLibrary.indexOf(i), 1);
-            displayBooksAgain();
-        })
-        cardClone.appendChild(removeBtn);
-        if (myLibrary[i].read == 'yes') {
-            cardClone.style.boxShadow = '#9ffbc9 0px 1px 4px';
-        } else cardClone.style.boxShadow = '#fa9da4 0px 1px 4px';
-        document.getElementById('card-container').appendChild(cardClone);
-    }
-}
+        const card = document.createElement('div');
+        card.classList.add('displayed-card');
+        const titles = document.createElement('div');
+        titles.classList.add('titles');
+        titles.innerHTML = myLibrary[i].title;
+        card.appendChild(titles);
 
-function displayBooksAgain() {
-    removeCards();
-    displayBooks();
+        const subtitleContainer = document.createElement('div');
+        subtitleContainer.classList.add('sub-title-container');
+        card.appendChild(subtitleContainer);
+        const subtitleOne = document.createElement('div');
+        subtitleOne.innerHTML = `Author: ${myLibrary[i].author}`;
+        const subtitleTwo = document.createElement('div');
+        subtitleTwo.innerHTML = `Number of pages: ${myLibrary[i].pages}`;
+        
+        const subtitleThree = document.createElement('div');
+        const subtitleThreeHeading = document.createElement('div');
+        subtitleThreeHeading.innerHTML = 'Mark as read: ';
+        subtitleThree.appendChild(subtitleThreeHeading);      
+        
+        const subtitleThreeSlider = document.createElement('label');
+        subtitleThreeSlider.classList.add('switch');
+        const checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+        subtitleThreeSlider.appendChild(checkbox);
+        const sliderSpan = document.createElement('span');
+        sliderSpan.classList.add('slider');
+        subtitleThreeSlider.appendChild(sliderSpan);
+        subtitleThree.appendChild(subtitleThreeSlider);
+        
+        subtitleContainer.appendChild(subtitleOne);
+        subtitleContainer.appendChild(subtitleTwo);
+        subtitleContainer.appendChild(subtitleThree);      
+        
+        const removeBtn = document.createElement('span');
+        removeBtn.innerHTML = '&#11199';
+        removeBtn.addEventListener('click', () => {
+            myLibrary.splice(i, 1);
+            /*card.classList.add('template');*/
+            removeCards();
+            displayBooks();
+        })
+        titles.appendChild(removeBtn);
+
+        checkbox.addEventListener('click', () => {
+            if (checkbox.checked == false) {
+                card.style.boxShadow = '#fa9da4 0px 1px 4px';
+                myLibrary[i].read = 'no';
+            } else {
+                card.style.boxShadow = '#9ffbc9 0px 1px 4px';
+                myLibrary[i].read = 'yes';
+            }
+        })
+
+        if (myLibrary[i].read == 'yes') {
+            card.style.boxShadow = '#9ffbc9 0px 1px 4px';
+            checkbox.checked = true;
+        } else {
+            card.style.boxShadow = '#fa9da4 0px 1px 4px';
+            checkbox.checked = false;
+        }
+        document.getElementById('card-container').appendChild(card);
+    }
 }
 
 const titleInput = document.getElementById('title');
